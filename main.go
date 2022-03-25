68b835f137834c2426620daa45e498435a0d455c
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2022-03-25 15:05:18 +0700

    Create sample metrics exporter

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func recordMetrics() {
+	for {
+		// dat, err := explicit()
+
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(15 * time.Second)
+			continue
+		}
+		opsProcessed.Inc()
+		time.Sleep(2 * time.Second)
+	}
+}
+
+var reg = prometheus.NewRegistry()
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func init() {
+	reg.MustRegister(opsProcessed)
+}
+
 func main() {
+	go recordMetrics()
+
 	http.Handle("/something", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 	http.ListenAndServe(":9191", nil)
 }
